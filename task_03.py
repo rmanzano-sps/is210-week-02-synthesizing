0074b8a003e86882b520a83c2ce4daa8c4b08b8d
rmanzano-sps <reynaldo.manzano@spsmail.cuny.edu>
2015-09-07 01:22:04 +0000

    Here are the updated and modified files requested from tasks

--- task_03.py
+++ task_03.py
@@ -1,7 +1,8 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""I have added a docstring line"""
+
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
